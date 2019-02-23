ff67f55301fce0e7fda24ace99828e921b1b2de4
Marlon Raphael <raphael27atm@gmail.com>
2019-02-23 11:06:31 -0300

    Fix styles

--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -1,40 +1,70 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import { dateYYYYMMDDHHMMSS } from '../../utils/utils';
 import PostActions from '../PostActions';
 
+
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 // Styles
-//import { styles } from './styles'
+import { styles } from './styles'
 
 class Post extends Component {
+
   render() {
-    const {post} = this.props;
+
+    const {post, classes} = this.props;
     return (
-      <div className="card post-card" key={post.id}>
-        <div className="card-header">
-          {post.category}
-        </div>
-        <div className="card-body">
-          <h3 className="blog-post-title">{post.title}</h3>
-          <p className="blog-post-meta">{dateYYYYMMDDHHMMSS(post.timestamp)}, by {post.author}</p>
-          <p>{post.body.substring(0,150)} ...</p>
-          <hr />
-          <p >
-            {post.voteScore >= 0 ? (
-                <i className="far fa-thumbs-up"></i>
-            ):(
-                <i className="far fa-thumbs-down"></i>
-            )} {post.voteScore}&nbsp;&nbsp;&nbsp;&nbsp;<i className="far fa-comments"></i> {post.commentCount} {post.commentCount > 1 ? 'comments': 'comment'}
-          </p>
-          <PostActions
-              postId = {post.id}
+      <React.Fragment>
+        <Card className={classes.card} key={post.id}>
+          <CardHeader
+            title={post.title}
+            subheader={`${dateYYYYMMDDHHMMSS(post.timestamp)}, by ${post.author}`}
           />
-        </div>
-        <div className="card-footer text-muted">
-          <Link to = {`/${post.category}/${post.id}`}>View details</Link>
-        </div>
-      </div>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {post.category}  
+              </Typography>
+              <Typography component="p">
+                {post.body.substring(0,150)} ...
+              </Typography>
+              <Typography component="p">
+              {post.voteScore >= 0 ? (
+                <i className="far fa-thumbs-up"></i>
+                ):(
+                    <i className="far fa-thumbs-down"></i>
+                )} {post.voteScore}&nbsp;&nbsp;&nbsp;&nbsp;<i className="far fa-comments"></i> {post.commentCount} {post.commentCount > 1 ? 'comments': 'comment'}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.cardAction}>
+            <Button variant="contained" color="primary" className={classes.button}>
+              Primary
+            </Button>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Link to = {`/${post.category}/${post.id}`}>
+              <Button size="small" color="primary">
+                View details
+              </Button>
+            </Link>
+            <PostActions
+              postId = {post.id}
+            />
+          </CardActions>
+        </Card>
+      </React.Fragment>
     )
   }
 }
-export default Post
+export default styles(Post)
--- frontend/src/components/PostActions/index.js
+++ frontend/src/components/PostActions/index.js
@@ -2,58 +2,109 @@ import React, { Component } from 'react';
 import * as API from '../../utils/api';
 import { connect } from 'react-redux';
 import { addPost, removePost, increasePostScore, decreasePostScore} from '../../redux/actions';
+
+// Material ui
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
+// Components
 import EditPost from '../EditPost';
+
+// styles 
+import { styles } from './styles'
 class PostActions extends Component {
-    constructor() {
-        super();
-        this.state = {
-        }
+  state = {
+    open: false,
+  };
+  handleDelete(type, id){
+      if(type === 'post'){
+          this.props.removePost(id);
+      }
+  }
+  increasePostScore(id){
+      API.increasePostScore(id).then(results =>{
+          this.props.increasePostScore(id)
+      })
+  }
+  decreasePostScore(id){
+      API.decreasePostScore(id).then(results =>{
+          this.props.decreasePostScore(id)
+      })
+  }
+  removePost(id){
+      API.removePost(id).then(results =>{
+          this.props.removePost(id)
+      })
+  }
+  handleOpen = () => {
+      this.setState({ open: true });
     };
-    handleDelete(type, id){
-        if(type === 'post'){
-            this.props.removePost(id);
-        }
-    }
-    increasePostScore(id){
-        API.increasePostScore(id).then(results =>{
-            this.props.increasePostScore(id)
-        })
-    }
-    decreasePostScore(id){
-        API.decreasePostScore(id).then(results =>{
-            this.props.decreasePostScore(id)
-        })
-    }
-    removePost(id){
-        API.removePost(id).then(results =>{
-            this.props.removePost(id)
-        })
-    }
-    render() {
-        const { post } = this.props;
-        return (
-            <div>
-                <button className="btn btn-sm btn-info" onClick = {event => this.increasePostScore(post.id)}>Like</button>
-                <button className="btn btn-sm btn-info" onClick = {event => this.decreasePostScore(post.id)}>Dislike</button>
-                <button className="btn btn-sm btn-primary" data-toggle="modal" data-target="#editPostModal">Edit</button>
-                <button type="button" className="btn btn-sm btn-danger" onClick = {event => this.removePost(post.id)}>Delete</button>
-                <EditPost
-                    post = {post}
-                    title = 'Edit a post' 
-                />  
+  
+    handleClose = () => {
+      this.setState({ open: false });
+    };
+  render() {
+      const { post, classes } = this.props;
+      return (
+        <div>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick = {event => this.increasePostScore(post.id)} 
+            className={classes.button}
+          >
+            Like
+          </Button>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick = {event => this.decreasePostScore(post.id)} 
+            className={classes.button}
+          >
+            Dislike
+          </Button>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick={this.handleOpen}
+            className={classes.button}
+          >
+            Edit
+          </Button>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick = {event => this.removePost(post.id)}
+            className={classes.button}
+          >
+            Delete
+          </Button>
+          <Modal
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            open={this.state.open}
+            onClose={this.handleClose}
+          > 
+            <div className={classes.paper}>
+              <EditPost
+                post = {post}
+                title = 'Edit a post' 
+              />  
             </div>
-        )
-    }
+          </Modal>
+        </div>
+    )
+  }
 }
 const mapStateToProps = ({posts,comments},{postId}) =>{
-    return {
-        post : posts.filter(p => p.id === postId)[0],
-    }
+  return {
+      post : posts.filter(p => p.id === postId)[0],
+  }
 }
 const mapDispatchToProps = dispatch =>({
-    addPost: (data) => dispatch(addPost(data)),
-    removePost: (id) => dispatch(removePost(id)),
-    increasePostScore: (id) => dispatch(increasePostScore(id)),
-    decreasePostScore: (id) => dispatch(decreasePostScore(id)),
+  addPost: (data) => dispatch(addPost(data)),
+  removePost: (id) => dispatch(removePost(id)),
+  increasePostScore: (id) => dispatch(increasePostScore(id)),
+  decreasePostScore: (id) => dispatch(decreasePostScore(id)),
 })
-export default connect(mapStateToProps,mapDispatchToProps)(PostActions)
+export default connect(mapStateToProps,mapDispatchToProps)(styles(PostActions))
