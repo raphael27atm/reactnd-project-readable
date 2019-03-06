8df2cceb6737406c65e54d68406cc8b8ed1eaf51
Marlon Raphael <raphael27atm@gmail.com>
2019-03-06 15:54:22 -0300

    Fix identation

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -31,7 +31,7 @@ export const username = (str = '') => {
 
 export const uuid = () => {
   return Math.random().toString(36).substring(2) +
-      (new Date()).getTime().toString(36)
+    (new Date()).getTime().toString(36)
 }
 
 export const dateYYYYMMDDHHMMSS = (input) => {
